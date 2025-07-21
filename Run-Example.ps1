f439759ee8ac4cff4d4148196b4ddeca4e6ad4db
Christoph Oberle <christoph.oberle@oberle-it-beratung.de>
2025-07-21 18:47:36 +0200

    Update Run-Example.ps1

--- Run-Example.ps1
+++ Run-Example.ps1
@@ -2,12 +2,21 @@
 $separator = [IO.Path]::DirectorySeparatorChar
 
 # scan a directory for XML files
-./Scan-Directory.ps1 -Path .. -Include *.xml -Depth 100 |
+./Scan-Directory.ps1 -Path /Users -Include *.xml -Depth 100 |
         <# write results to xmlFiles.csv #>
         Tee-Object -FilePath "xmlFiles.csv" |
         <# write file info in a different format #>
-        ForEach-Object -Begin {$i = 0} -Process {
+        ForEach-Object -Begin {
+            $i = 0
+            $start = Get-Date
+        } -Process {
+            # all directories are gathered into the last parameter $dir as an array
             $filename, $leafbase, $extension, $dirCount, $dirs = ($_ -split "; ")
-            Write-Host "$i`: $leafbase$extension $dircount $($dirs -join $separator)"
+            Write-Host "$i`: `"$leafbase$extension`" $dircount `"$($dirs -join $separator)`""
             $i++
+        } -End {
+            $end = Get-Date
+            Write-Host "$i files found"
+            Write-Host "Execution time: $($end - $start)"
         }
+
